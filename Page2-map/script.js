b145ecf7ab77c55a0d85a8b89fc1c8036230eeb3
Navnit <178172653+Navnit-07@users.noreply.github.com>
2024-12-02 00:20:41 +0530

    update the changes

--- Page2-map/script.js
+++ Page2-map/script.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", () => {
     const googleSignInButton = document.getElementById("search");
   
     googleSignInButton.addEventListener("click", () => {
       window.location.href = "/search"; // Redirect to the desired page
     });
-=======
-document.addEventListener("DOMContentLoaded", () => {
-    const googleSignInButton = document.getElementById("search");
-  
-    googleSignInButton.addEventListener("click", () => {
-      window.location.href = "/search"; // Redirect to the desired page
-    });
->>>>>>> e7c8f5e3f30e2aefd2ca1ac4f4949c1611e4949a
   });
