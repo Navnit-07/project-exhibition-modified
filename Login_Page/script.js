3030c68b51fe321faf090ab23044540e77be5e2a
MD EAMAN ADEEP <144486159+Eaman006@users.noreply.github.com>
2024-12-01 17:37:38 +0530

    google auth was added

--- Login_Page/script.js
+++ Login_Page/script.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const googleSignInButton = document.getElementById("googleSignIn");
   
     googleSignInButton.addEventListener("click", () => {
-      window.location.href = "/home"; // Redirect to the desired page
+      window.location.href = "/auth/google"; // Redirect to the desired page
     });
   });
